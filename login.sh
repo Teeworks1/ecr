8ae9f7aa2abb3d02e545c76080b38050437c087d
ochuko.ebireri <ebireriochuko90@gmail.com>
2024-04-09 06:38:17 -0600

    djiejdj

--- login.sh
+++ login.sh
@@ -1,74 +1,40 @@
-- name: Identify Values Files
-  id: identify-values-files
-  run: |
-    #!/bin/bash
+#!/bin/bash
+echo "Today is:"
+cal
+date
 
-    echo "Searching for values*.yaml files..."
-    values_files=$(find . -type f -name "values*.yaml")
+env_var="123456"
+echo $env_var
 
-    if [ -n "$values_files" ]; then
-        echo "Values*.yaml files found:"
-        echo "$values_files"
-        echo "::set-output name=values_files::$values_files"
-    else
-        echo "No values*.yaml files found."
-        exit 1
-    fi
+echo "enter first string"
+read st1
 
-    overrideTag=""
-    image=""
-    latestTags=()
+echo "enter 2nd string"
+read st2
 
-    for file in $values_files; do
-        echo "Processing $file..."
-        while IFS= read -r line; do
-            if [[ $line =~ image:\ (.+) ]]; then
-                image="${BASH_REMATCH[1]}"
-                echo "Image: $image"
-            fi
-            if [[ $line =~ overrideTag:\ (.+) ]]; then
-                overrideTag="${BASH_REMATCH[1]}"
-                echo "Override Tag: $overrideTag"
-                echo "Image: $image"
-                # If overrideTag is found, no need to search for latestTag
-                latestTags=()  # Clearing the latestTags array
-                continue
-            fi
-            if [[ $line =~ latestTag:\ (.+) ]]; then
-                if [[ -z "$overrideTag" ]]; then
-                    latestTag+=("${BASH_REMATCH[1]}") # Append the latestTag to the array
-                    echo "Latest Tag: ${BASH_REMATCH[1]}"
-                fi
-            fi
-        done < "$file"
-        
-        # Check if overrideTag is found before breaking the loop
-        if [[ -n "$overrideTag" ]]; then
-            break
-        fi
-    done
+if [ $st1 == $st2];
+then 
+     echo "strings match"
+else 
+     echo "strings dont match"
+fi
 
-    # Set outputs
-    echo "::set-output name=overrideTag::$overrideTag"
-    echo "::set-output name=image::$image"
-    echo "::set-output name=latestTags::$latestTag"
+car=('mercedes' 'Toyota' 'Tesla')
 
-- name: Debug Step Output
-        run:  echo "Step output: ${{ steps.identify-values-files.outputs.values_files }}"
+echo "${car[@]}"
 
-- name: Login to Amazon ECR
-  run: |
-    images=$(yq eval-all '.image' ${{ steps.identify-values-files.outputs.values_files }})
-    for image_uri in $images; do
-        account_id=$(echo "$image_uri" | cut -d'.' -f1)
-        region=$(echo "$image_uri" | cut -d'.' -f4)   
-        echo "Logging in to Amazon ECR for image: $image_uri"
-        aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$account_id.dkr.ecr.$region.amazonaws.com"
-    done
+names=("Tim" "Jim" "Ming")
+age=(12 24 31 40)
+ if  [ ${#names[@]} -eq ${#age[@]} ]; then
+   
+echo "length is the same"
+else
+ echo "not the same"
+ fi
+function  funcheck()
+{
+    verticalValue="I LOVE MAC"
 
-- name: List ECR Repositories
-  id: list-ecr-repos
-  run: |
-    echo "Listing ECR repositories..."
-    aws ecr describe-repositories --query 'repositories[*].[repositoryName, repositoryUri]'
-    echo "::set-output name=ecr_repositories::$(aws ecr describe-images --query 'repositories[*].[repositoryName, repositoryUri, imageTags[*]]' --output table)"
+}
+ funcheck
+ echo $verticalValue
--- tee.sh
+++ tee.sh
@@ -1,15 +1,64 @@
-- name: List ECR Repos
+- name: Identify Values Files
+        id: identify-values-files
+        run: |
+          echo "Searching for values*.yaml files..."
+          # Get the paths of the changed files
+          changed_files="${{ steps.diff.outputs.diff }}"
+
+          # Filter the paths to include only values*.yaml files
+          values_files=$(echo "$changed_files" | grep 'values.*\.yaml')
+
+          if [ -n "$values_files" ]; then
+              echo "Values*.yaml files found:"
+              echo "$values_files"
+              echo "::set-output name=values_files::$values_files"
+          else
+              echo "No values*.yaml files found."
+              exit 1
+          fi
+       
+          overrideTag=""
+          image=""
+          latestTags=()
+
+          for file in $values_files; do
+              echo "Processing $file..."
+              while IFS= read -r line; do
+                  if [[ $line =~ image:\ (.+) ]]; then
+                      image="${BASH_REMATCH[1]}"
+                      echo "Image: $image"
+                  fi
+                  if [[ $line =~ overrideTag:\ (.+) ]]; then
+                      overrideTag="${BASH_REMATCH[1]}"
+                      echo "Override Tag: $overrideTag"
+                      echo "Image: $image"
+                      latestTags=()  # Clearing the latestTags array
+                      continue
+                  fi
+                  if [[ $line =~ latestTag:\ (.+) ]]; then
+                      if [[ -z "$overrideTag" ]]; then
+                          latestTags+=("${BASH_REMATCH[1]}") # Append the latestTag to the array
+                          echo "Latest Tag: ${BASH_REMATCH[1]}"
+                      fi
+                  fi
+              done < "$file"
+          done
+
+          # Set outputs
+          echo "::set-output name=overrideTag::$overrideTag"
+          echo "::set-output name=image::$image"
+          echo "::set-output name=latestTags::$latestTags"
+          - name: List ECR Repos
   run: |
     echo "Listing ECR repositories..."
     ecr_repositories=$(aws ecr describe-repositories --query 'repositories[*].[repositoryName, repositoryUri]' --output json | jq -r 'unique')
     echo "::set-output name=ecr_repositories::$ecr_repositories"
-    echo "$ecr_repositories"
 
 - name: Describe ECR Images
   run: |
     # Get the output from the List ECR Repos step
     # Iterate through each repository
-    ecr_repositories_output="${{ steps.list_ecr_repos.outputs.ecr_repositories }}"
+    #ecr_repositories_output="${{ steps.list_ecr_repos.outputs.ecr_repositories }}"
     echo "$ecr_repositories" | jq -r '.[] | @tsv' | while IFS=$'\t' read -r repo_name repo_uri; do
       echo "Repo Name: $repo_name, Repo URI: $repo_uri"
       if [ -n "$repo_uri" ]; then
